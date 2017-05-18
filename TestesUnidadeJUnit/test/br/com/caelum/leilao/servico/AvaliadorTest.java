6a16049a0c8713b667d17d5ba5790d1a1ff85b7a
elisandro <elisandroluis@gmail.com>
2017-05-18 20:31:10 -0300

    Inicial

---
+++ TestesUnidadeJUnit/test/br/com/caelum/leilao/servico/AvaliadorTest.java
@@ -0,0 +1,81 @@
+package br.com.caelum.leilao.servico;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.dominio.Usuario;
+
+public class AvaliadorTest {
+
+    @Test
+    public void deveEntenderLancesEmOrdemCrescente() {
+        // cenario: 3 lances em ordem crescente
+        Usuario joao = new Usuario("Joao");
+        Usuario jose = new Usuario("José");
+        Usuario maria = new Usuario("Maria");
+
+        Leilao leilao = new Leilao("Playstation 3 Novo");
+
+        leilao.propoe(new Lance(maria,250.0));
+        leilao.propoe(new Lance(joao,300.0));
+        leilao.propoe(new Lance(jose,400.0));
+
+        // executando a acao
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+
+        // comparando a saida com o esperado
+        double maiorEsperado = 400;
+        double menorEsperado = 250;
+
+        assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.0001);
+        assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.0001);
+    }
+    
+    @Test
+    public void deveCalcularAMedia() {
+    	 // cenario: 3 lances em ordem crescente
+        Usuario joao = new Usuario("Joao");
+        Usuario jose = new Usuario("José");
+        Usuario maria = new Usuario("Maria");
+
+        Leilao leilao = new Leilao("Playstation 3 Novo");
+
+        leilao.propoe(new Lance(maria,100.0));
+        leilao.propoe(new Lance(joao,200.0));
+        leilao.propoe(new Lance(jose,300.0));
+
+        // executando a acao
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+
+        // comparando a saida com o esperado
+        double mediaEsperada = 200;
+
+        assertEquals(mediaEsperada, leiloeiro.getMedia(), 0.0001);
+    }
+    
+    @Test
+    public void deveEntenderUnicoLance() {
+        // cenario: 3 lances em ordem crescente
+        Usuario joao = new Usuario("Joao");
+
+        Leilao leilao = new Leilao("Playstation 3 Novo");
+
+        leilao.propoe(new Lance(joao,250.0));
+
+        // executando a acao
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+
+        // comparando a saida com o esperado
+        double maiorEsperado = 250;
+        double menorEsperado = 250;
+
+        assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.0001);
+        assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.0001);
+    }
+}
