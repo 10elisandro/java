285884e8fe5d38c51868c6ce11d6de7769d3cdf1
elisandro <elisandroluis@gmail.com>
2017-05-19 23:47:03 -0300

    Ajustes

--- ProjetoJavaWeb/src/config/Conexao.java
+++ ProjetoJavaWeb/src/config/Conexao.java
@@ -1,5 +1,6 @@
 package config;
 
+import java.net.URI;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
@@ -10,14 +11,28 @@ public class Conexao {
 
 	private static Connection getConnection() throws Exception {
 		if (con == null) {
-			Class.forName("org.postgresql.Driver").newInstance();
-			con = DriverManager.getConnection("jdbc:postgresql://192.168.0.3/projeto_java_web?user=USUARIO&password=SENHA");
+			URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+		    String username = dbUri.getUserInfo().split(":")[0];
+		    String password = dbUri.getUserInfo().split(":")[1];
+		    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+		    con = DriverManager.getConnection(dbUrl, username, password);
+		    
+		    criarTabelas();
 		}
+		
 		return con;
 	}
 	
 	public static Statement getStatement() throws Exception {
 		Connection con = Conexao.getConnection();
 		return con.createStatement();
+	}
+	
+	private static void criarTabelas() throws Exception {
+		Statement stmt = getStatement();
+        stmt.executeUpdate("DROP TABLE IF EXISTS produtos");
+        stmt.executeUpdate("CREATE TABLE produtos(codigo serial, id_categoria integer, nome varchar, descricao varchar)");
 	}
 }
